2d967dedee1e4dd216ddadfb81fb8ad347a7d685
oh_jun <vhzkclq0705@naver.com>
2022-10-24 16:39:30 +0900

    메인 화면 UI 구현(didSet, willSet 사용)

--- Step3/Step3/ViewController.swift
+++ Step3/Step3/ViewController.swift
@@ -9,11 +9,108 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var itemCountLabel: UILabel!
+    @IBOutlet weak var totalCostLabel: UILabel!
+    
+    // MARK: - Property
+    
+    var itemCount: Int = 0 {
+        didSet {
+            if self.itemCount < 0 {
+                self.itemCount = 0
+            }
+            self.itemCountLabel.text = "\(self.itemCount) Items"
+        }
+        willSet {
+            self.itemCount += newValue
+        }
+    }
+    
+    var totalCost: Int = 0 {
+        didSet {
+            if self.totalCost < 0 {
+                self.totalCost = 0
+            }
+            
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            let cost = formatter.string(from: self.totalCost as NSNumber)
+            
+            self.totalCostLabel.text = cost
+        }
+        willSet {
+            self.totalCost += newValue
+        }
+    }
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureVC()
     }
-
+    
+    // MARK: - Func
+    
+    private func configureVC() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    private func updateTotalCost(_ cost: Int, _ isPlus: Bool) {
+        self.totalCost = isPlus ? self.totalCost + cost : self.totalCost - cost
+        self.itemCount = isPlus ? self.itemCount + 1 : self.itemCount - 1
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func didTapClearButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didTapOrderButton(_ sender: Any) {
+        
+    }
 
 }
 
+// MARK: - TableView delegate & dataSource
+
+extension ViewController: UITableViewDelegate,
+                          UITableViewDataSource {
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int)
+    -> Int
+    {
+        return 20
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell
+    {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "Cell",
+            for: indexPath) as? Cell else {
+            return UITableViewCell()
+        }
+        
+        cell.updateCell(indexPath.row)
+        cell.plusButtonTapHandler = { cost in
+            self.updateTotalCost(cost, true)
+        }
+        cell.minusButtonTapHandler = { cost in
+            self.updateTotalCost(cost, false)
+        }
+        
+        return cell
+    }
+    
+}
