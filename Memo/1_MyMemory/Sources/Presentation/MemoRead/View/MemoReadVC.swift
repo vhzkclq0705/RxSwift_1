bb564391c9ae4244bf620e94f92703dee7aacff5
oh_jun <vhzkclq0705@naver.com>
2022-11-06 21:56:49 +0900

    MemoReadVC, ViewModel 수정

--- Memo/1_MyMemory/Sources/Presentation/MemoRead/View/MemoReadVC.swift
+++ Memo/1_MyMemory/Sources/Presentation/MemoRead/View/MemoReadVC.swift
@@ -14,39 +14,164 @@ class MemoReadVC: UIViewController {
 
     // MARK: - UI
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var contentsLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var updateButton: UIBarButtonItem!
     @IBOutlet weak var deleteButton: UIBarButtonItem!
     
+    lazy var cameraButton = UIBarButtonItem(
+        barButtonSystemItem: .camera,
+        target: self,
+        action: nil)
+    
+    lazy var completeButton = UIBarButtonItem(
+        barButtonSystemItem: .done,
+        target: self,
+        action: nil)
+    
     // MARK: - Property
     
-    var viewModel: MemoReadViewModel?
+    var viewModel: MemoReadViewModel!
     var disposeBag = DisposeBag()
+    var tempTitle: String? = ""
+    var tempButton1 = UIBarButtonItem()
+    var tempButton2 = UIBarButtonItem()
     
     // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureBindings()
+        BindViewModel()
+        configureViewController()
+    }
+    
+    // MARK: - Bind
+    
+    func BindViewModel() {
+        let input = MemoReadViewModel.Input(
+            updateButtonDidTapEvent: updateButton.rx.tap.asObservable(),
+            deleteButtonDidTapEvent: deleteButton.rx.tap.asObservable(),
+            cameraButtonDidTapEvent: cameraButton.rx.tap.asObservable(),
+            completeButtonDidTapEvent: completeButton.rx.tap.asObservable(),
+            title: titleTextField.rx.text.orEmpty.asObservable(),
+            contents: contentsTextView.rx.text.asObservable())
+        
+        let output = viewModel.transform(input: input)
+        
+        output.title
+            .bind(to: titleTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.contents
+            .bind(to: contentsTextView.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.date
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.title = $0
+            })
+            .disposed(by: disposeBag)
+        
+        output.imgData
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.imageView.image = UIImage(data: $0 ?? Data())
+            })
+            .disposed(by: disposeBag)
+        
+        output.update
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.isUpdate(true)
+            })
+            .disposed(by: disposeBag)
+        
+        output.complete
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.isUpdate(false)
+                self?.viewModel.updateMemo()
+            })
+            .disposed(by: disposeBag)
+        
+        output.delete
+            .subscribe(onNext: { [weak self] in
+                self?.presentAlert()
+            })
+            .disposed(by: disposeBag)
+        
+        output.camera
+            .subscribe(onNext: { [weak self] in
+                self?.pickImage()
+            })
+            .disposed(by: disposeBag)
     }
     
     // MARK: - Configure
     
-    func configureBindings() {
-        viewModel?.output.memo
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] in
-                if $0.imageData != nil {
-                    self?.img.image = UIImage(data: $0.imageData!)
-                }
-                self?.subject.text = $0.title
-                self?.contents.text = $0.contents
-                self?.title = $0.regdate.formatToString(true)
-            })
-            .disposed(by: disposeBag)
+    func configureViewController() {
+        titleTextField.layer.borderWidth = 1
+        titleTextField.layer.borderColor = UIColor.lightGray.cgColor
+        contentsTextView.layer.borderWidth = 1
+        contentsTextView.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    // MARK: - Func
+    
+    func isUpdate(_ bool: Bool) {
+        titleTextField.isEnabled = bool
+        contentsTextView.isEditable = bool
+        titleTextField.backgroundColor = bool ? UIColor.systemGray5 : UIColor.white
+        contentsTextView.backgroundColor = bool ? UIColor.systemGray5 : UIColor.white
+        
+        if bool {
+            tempTitle = title
+            title = "수정"
+            tempButton1 = updateButton
+            tempButton2 = deleteButton
+            self.navigationItem.rightBarButtonItems?[1] = cameraButton
+            self.navigationItem.rightBarButtonItems?[0] = completeButton
+        } else {
+            title = tempTitle
+            self.navigationItem.rightBarButtonItems?[1] = tempButton1
+            self.navigationItem.rightBarButtonItems?[0] = tempButton2
+        }
+    }
+    
+    func pickImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false)
     }
 
+}
+
+// MARK: - ImagePickerControllerDelegate
+
+extension MemoReadVC: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        viewModel.setImage((info[.editedImage] as? UIImage)?.pngData())
+            .subscribe(onNext: { [weak self] in
+                self?.imageView.image = UIImage(data: $0!)
+            })
+            .disposed(by: disposeBag)
+        
+        picker.dismiss(animated: true)
+    }
+    
+}
+
+// MARK: - NavigationControllerDelegate
+
+extension MemoReadVC: UINavigationControllerDelegate {
+    
 }
--- Memo/1_MyMemory/Sources/Presentation/MemoRead/ViewModel/MemoReadViewModel.swift
+++ Memo/1_MyMemory/Sources/Presentation/MemoRead/ViewModel/MemoReadViewModel.swift
@@ -9,23 +9,118 @@ import Foundation
 
 import RxSwift
 
-class MemoReadViewModel {
+protocol MemoReadViewModelType: ViewModelType {
+    var storage: MemoStorageType { get }
+}
+
+class MemoReadViewModel: MemoReadViewModelType {
+    
+    // MARK: - Input
+    
+    struct Input {
+        let updateButtonDidTapEvent: Observable<Void>
+        let deleteButtonDidTapEvent: Observable<Void>
+        let cameraButtonDidTapEvent: Observable<Void>
+        let completeButtonDidTapEvent: Observable<Void>
+        let title: Observable<String>
+        let contents: Observable<String?>
+    }
     
     // MARK: - Ouput
     
     struct Output {
-        let memo: Observable<MemoData>
+        let title: Observable<String>
+        let contents: Observable<String?>
+        let date: Observable<String>
+        let imgData: Observable<Data?>
+        let update: Observable<Void>
+        let complete: Observable<Void>
+        let delete: Observable<Void>
+        let camera: Observable<Void>
     }
     
     // MARK: - Property
     
-    let output: Output
+    var storage: MemoStorageType
     var disposeBag = DisposeBag()
+    private var memo: Observable<MemoData>
+    private var original: MemoData!
+    private var title: String
+    private var contents: String?
+    private var imgData: Data?
     
     // MARK: - Init
     
     init(_ memo: MemoData) {
-        output = Output(memo: Observable.just(memo))
+        self.storage = Storage.shared
+        self.memo = Observable.just(memo)
+        self.original = memo
+        self.title = ""
+        self.contents = nil
+        self.imgData = nil
+    }
+    
+    // MARK: - Bind
+    
+    func transform(input: Input) -> Output {
+        memo.subscribe(onNext: {
+            self.original = $0
+        })
+        .disposed(by: disposeBag)
+        
+        input.title
+            .subscribe(onNext: { [weak self] in
+                self?.title = $0
+            })
+            .disposed(by: disposeBag)
+            
+        input.contents
+            .subscribe(onNext: { [weak self] in
+                self?.contents = $0
+            })
+            .disposed(by: disposeBag)
+        
+        let title = memo
+            .map { $0.title }
+ 
+        let contents = memo
+            .map { $0.contents }
+        
+        let imgData = memo
+            .map { $0.imageData }
+        
+        let date = memo
+            .map { $0.regdate.formatToString(true) }
+        
+        let update = input.updateButtonDidTapEvent
+                
+        let complete = input.completeButtonDidTapEvent
+        
+        let deleteAlert = input.deleteButtonDidTapEvent
+        
+        let camera = input.cameraButtonDidTapEvent
+        
+        return Output(
+            title: title,
+            contents: contents,
+            date: date,
+            imgData: imgData,
+            update: update,
+            complete: complete,
+            delete: deleteAlert,
+            camera: camera)
+    }
+    
+    // MARK: - Func
+    
+    func setImage(_ imgData: Data?) -> Observable<Data?> {
+        self.imgData = imgData
+        
+        return Observable.just(imgData)
+    }
+    
+    func updateMemo() {
+        storage.update(memo: original, title: title, contents: contents, imgData: imgData)
     }
     
 }
